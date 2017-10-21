63f66a11ce8667d5f381f3854db3814c57ac4d84
Camilo Fernandes <camilo.analista@gmail.com>
2017-10-21 20:54:57 -0200

    print value on template

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+
+    name = "Camilo"
+    languages = ['Python', 'PHP', 'Java', 'Ruby']
+
+    context = {
+        'name': name,
+        'languages': languages
+    }
+
+    return render(request, 'blog/home.html', context)
