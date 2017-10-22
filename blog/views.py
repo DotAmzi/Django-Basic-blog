3bb400516ef2d412619e669d6e571074c28147d4
Camilo Fernandes <camilo.analista@gmail.com>
2017-10-22 01:48:55 -0200

    show all posts or published

--- blog/views.py
+++ blog/views.py
@@ -9,24 +9,25 @@ def home(request):
     name = "Camilo"
     # categories = ['PHP', 'Java', 'Ruby']
     # for category in categories:
-    #     Category.objects.create(name=category)
+        # Category.objects.create(name=category)
 
     all_categories = Category.objects.all()
-    category_python = Category.objects.get(name='Python')
-    post = Post.objects.get(pk=1)
+    category = Category.objects.get(name='Ruby')
+    posts = Post.objects.filter(status="Published")
+    # Para todos Post.objects.all()
 
     # post = Post()
-    # post.name = "My first very post"
-    # post.content = "Content of my first post"
+    # post.name = "Show post 3"
+    # post.content = "Content of my  post"
     # post.status = "Published"
-    # post.category = category_python
-    #
+    # post.category = category
+
     # post.save()
 
     context = {
         'name': name,
         'categories': all_categories,
-        'post': post,
+        'posts': posts,
     }
 
     return render(request, 'blog/home.html', context)
