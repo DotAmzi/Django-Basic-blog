1452bce0ee64833a413ce6f2a28c283d0a071c02
Camilo Fernandes <camilo.analista@gmail.com>
2017-10-21 23:18:27 -0200

    save a data on relationship

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from blog.models import Category
+from blog.models import Category, Post
 
 # Create your views here.
 
@@ -12,10 +12,21 @@ def home(request):
     #     Category.objects.create(name=category)
 
     all_categories = Category.objects.all()
+    category_python = Category.objects.get(name='Python')
+    post = Post.objects.get(pk=1)
+
+    # post = Post()
+    # post.name = "My first very post"
+    # post.content = "Content of my first post"
+    # post.status = "Published"
+    # post.category = category_python
+    #
+    # post.save()
 
     context = {
         'name': name,
-        'categories': all_categories
+        'categories': all_categories,
+        'post': post,
     }
 
     return render(request, 'blog/home.html', context)
