981992c2fa3f4762cb2e55a11857552196468018
Camilo Fernandes <camilo.analista@gmail.com>
2017-10-23 17:35:26 -0200

    ajust user

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from blog.models import Category, Post
+from django.contrib.auth.admin import UserAdmin
+from blog.models import Category, Post, UserProfile, User
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
 
 class PostAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'status', 'category')
     search_fields = ['id', 'name', 'content']
     list_filter = ['status', 'category', 'created_at']
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
