e18d87f9bc7d8c1beaefdf453ca5dd921f7f097d
Camilo Fernandes <camilo.analista@gmail.com>
2017-10-24 13:19:53 -0200

    create a post form

--- blog/forms.py
+++ blog/forms.py
@@ -4,6 +4,15 @@ from blog.models import Post
 
 
 class PostForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+
     class Meta:
         model = Post
         fields = ['category', 'name', 'content', 'status']
